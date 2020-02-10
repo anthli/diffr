0ce45c753a0d03c4520f308bd22c4dedc9921bfb
Anthony Li <anthli1895@gmail.com>
2020-02-10 12:50:23 -0500

    Fixed formatting

--- src/main/kotlin/com/anthli/diffr/Diff.kt
+++ src/main/kotlin/com/anthli/diffr/Diff.kt
@@ -4,7 +4,8 @@ package com.anthli.diffr
  * Encapsulation of the type of operation and the involved text of a diff.
  */
 data class Diff(val op: Operation, val text: String) {
-  override fun toString(): String {return buildString {
+  override fun toString(): String {
+    return buildString {
       append(when (op) {
         Operation.EQUAL -> ""
         Operation.INSERT -> "+"
