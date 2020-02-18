0eb23298d7df1dc3f707ad01ce48bfae09bf53f1
Anthony Li <anthli1895@gmail.com>
2020-02-18 00:39:50 -0500

    Working on implementing a Diff serializer with kotlinx.serialization (code does not compile as of this change)

--- server/src/test/kotlin/com/anthli/diffr/serializer/DiffSerializerTest.kt
+++ server/src/test/kotlin/com/anthli/diffr/serializer/DiffSerializerTest.kt
@@ -16,22 +16,17 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.anthli.diffr
+package com.anthli.diffr.serializer
 
-import io.ktor.application.call
-import io.ktor.http.ContentType
-import io.ktor.response.respondText
-import io.ktor.routing.get
-import io.ktor.routing.routing
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
+import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.json.Json
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
-fun main() {
-  embeddedServer(Netty, 8080) {
-    routing {
-      get("/") {
-        call.respondText("Hello, world!", ContentType.Text.Html)
-      }
-    }
-  }.start()
+@UnstableDefault
+class DiffSerializerTest {
+  @Test
+  fun `test Diff serialization`() {
+    Assertions.assertNotNull(Json.parse(DiffSerializer, "{op: '+', text: 'A'}"))
+  }
 }
